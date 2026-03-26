11ec5649e8165488ba964b1813cf02c0f233c7d4
zahra <zohrajbari2003@gmail.com>
2026-03-26 08:48:11 +0100

    update notification service

--- notification-service/src/middlewares/auth.js
+++ notification-service/src/middlewares/auth.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken');
 const fs = require('fs');
 const path = require('path');
 
-// قراءة الـ Public Key (نفسو اللي فـ كاع السيرفيسات)
 const publicKey = fs.readFileSync(path.join(__dirname, '../keys/publicKey.pem'), 'utf8');
 
 module.exports = (req, res, next) => {
@@ -13,7 +12,7 @@ module.exports = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, publicKey, { algorithms: ['RS256'] });
-        req.user = decoded; // كيحط معلومات المستخدم (Roles) فـ الطلب
+        req.user = decoded;
         next();
     } catch (err) {
         res.status(401).json({ message: "Token non valide" });
